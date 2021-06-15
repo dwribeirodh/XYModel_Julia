66399e11542888df5014ea820af73eda9b1553a1
dwribeirodh <66744366+dwribeirodh@users.noreply.github.com>
2021-06-15 08:42:58 -0500

    added setup.jl for downloading pkgs

---
+++ setup.jl
@@ -0,0 +1,15 @@
+# This script will install all necessary packages for use of the repository
+# XYModel_Julia.
+# Any questions, email ribei040@umn.edu
+
+Using Pkg
+
+Pkg.add("Distributions")
+Pkg.add("Colors")
+Pkg.add("ProgressBars")
+Pkg.add("Plots")
+Pkg.add("FiniteDifferences")
+Pkg.add("LinearAlgebra")
+Pkg.add("SpecialFunctions")
+Pkg.add("DelimitedFiles")
+Pkg.add("PyCall")
